3e655acc5382e07aac994fff22c49a2b89bca1bc
Caio Marins <caio.marins011@gmail.com>
2024-10-07 11:52:04 -0300

    primeira aula de js

---
+++ js/aula1/main.js
@@ -0,0 +1,36 @@
+// var idade = 18;
+// let nome = 'Caio';
+// const sobrenome = 'Marins';
+
+// console.log( idade, nome, typeof sobrenome);
+// console.log(`Meu nome é ${nome} ${sobrenome} e tenho ${idade} anos`);
+
+
+
+
+
+
+
+// let nome, nota1, nota2, media;
+
+// nome = 'Caio'
+// nota1 = 10 * 0.6
+// nota2 = 0 * 0.4
+// media = (nota1 + nota2) / 2
+
+// console.log(media);
+
+
+
+
+let valor1, valor2, troca;
+
+valor1 = 'a'
+valor2 = 'b'
+
+troca = valor1
+valor1 = valor2
+valor2= troca
+
+console.log(valor1);
+
