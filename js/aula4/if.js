9a4877e2b8342219f1264dd6885f922b9395d70c
Caio Marins <caio.marins011@gmail.com>
2024-10-14 11:14:16 -0300

    aula 4 if

---
+++ js/aula4/if.js
@@ -0,0 +1,24 @@
+// let idade = 16
+// if (idade>=16) {
+//     console.log(`Você pode votar`);
+// }
+// console.log(`Fim.`);
+
+////////////////////////////
+
+// let n1, n2, media
+// n1= 10
+// n2=7
+// faltas = 89
+
+// media = (n1 + n2) / 2
+
+// if (media > 8 && faltas <= 5) {
+//     console.log(`Parabénssss, você passou`);
+// } else {
+//     console.log (`Infelizmente não foi dessa vez`);
+// }
+
+
+///////////////////
+
---
+++ js/aula4/switch.js
@@ -0,0 +1,24 @@
+let atendimento = 3
+
+
+switch (atendimento) {
+    case 1:
+        console.log("1 ponto");
+        break;
+    case 2:
+        console.log("2 pontos");
+        break;
+    case 3:
+        console.log("3 pontos");
+        break;
+    case 4:
+        console.log("4 pontos");
+        break;
+    case 5:
+        console.log("5 pontos");
+        break;
+
+    default:
+        console.log("Sem Avaliação");
+        break;
+}
