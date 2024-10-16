281217d531d6d62e257f11df2b5087d57913bf18
Caio Marins <caio.marins011@gmail.com>
2024-10-16 11:45:58 -0300

    laços de repetição

---
+++ js/aula5/for.js
@@ -0,0 +1,11 @@
+// for(let contador = 0; contador < 10; contador+){
+//     console.log(contador);
+// }
+
+
+let sala = ['caio', 'luizao', 'luiz', 'eva', 'lele']
+
+for (let i = 0 ; i < sala.length; i++) {
+    console.log(`Olá, ${sala[i]}, seu R.A é ${i+1}`)
+    
+}
---
+++ js/aula5/while.js
@@ -0,0 +1,27 @@
+// let contador = 50;
+
+// while (contador >= 0 ) {
+
+//     console.log(`Contador: ${contador}`)
+//     contador -= 10
+    
+// }
+// console.log('fim');
+
+//////////////////////
+
+let contador = 11;
+let contador2 = 11;
+
+
+while (contador <=10) {
+    contador++
+    console.log(contador);
+}
+// testa e dpois executa
+console.log('------------------------------');
+// executa primeiro e depois testa
+do {
+    contador2++
+    console.log(contador2);
+} while (contador2 <=10);
