57da3e6a2870bb3bcc18c63221d1fa2525f77362
Caio Marins <caio.marins011@gmail.com>
2024-10-22 11:42:36 -0300

    Aula 7 js

---
+++ js/aula7/funcoes.js
@@ -0,0 +1,8 @@
+function calcularMedia (nota1 = 0, nota2 = 0) // passa parametro e set valor default caso não passe um dos parametros
+{
+    let media = (nota1 + nota2) / 2;
+    console.log(media);
+}
+
+
+calcularMedia(10,)
